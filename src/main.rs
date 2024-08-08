83cd2481d6627266618bf22866af88d14f0fa503
Bastian Oppermann <bastianoppermann1997@gmail.com>
2024-08-08 22:50:53 +0200

    Make host, port, and redis url configurable
    With environment variables.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,20 @@ use data_processor::{legacy_submit_data, submit_data};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let redis = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+    let redis_url = match std::env::var("REDIS_URL") {
+        Ok(url) => url,
+        Err(_) => {
+            eprintln!("Please set the REDIS_URL environment variable");
+            std::process::exit(1);
+        }
+    };
+    let redis = redis::Client::open(redis_url).unwrap();
+
+    let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .unwrap();
 
     HttpServer::new(move || {
         App::new()
@@ -11,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .service(submit_data)
             .service(legacy_submit_data)
     })
-    .bind(("127.0.0.1", 1234))?
+    .bind((host, port))?
     .run()
     .await
 }
